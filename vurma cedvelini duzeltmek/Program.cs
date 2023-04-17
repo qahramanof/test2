c4cda6225a6dc298ef0c80a78868364593440d89
unknown <kananng@code.edu.az>
2023-04-17 15:06:29 +0400

    vurma cedveli

---
+++ vurma cedvelini duzeltmek/Program.cs
@@ -0,0 +1,149 @@
+﻿//// VURMA CEDVELI DUZELTMETIN YOLU
+
+//for (int i = 1; i <= 10; i++)
+//{
+//    for (int j = 1; j <= 10; j++)
+
+//    {
+
+//        Console.WriteLine(i + "x" + j + "=" + (i * j));
+//    }
+//}
+
+
+
+//for (int i=1; i<=10;i++)
+//{
+//    for (int j = 1; j <= i; j++)
+//    {
+//        Console.Write(j);
+//    }
+//    Console.WriteLine("");
+
+//}
+
+
+
+//int[] arr = { 4, 78, 65, 23, 12, 1, 8 };
+//for(int i = 0;i < arr.Length; i++)
+//{
+//      for(int j =i+1;j<arr.Length;j++)
+//    {
+//        if (arr[j] < arr[i])
+//        {
+//            int continer = arr[i];
+//            arr[i] = arr[j];
+//            arr[j] = continer;
+//        }
+
+//    }
+//}
+
+
+
+
+
+//int[] arr = { 1, 90, 6, 43, 56, 5 };
+
+//for (int i = 0; i < arr.Length - 1; i++)
+//{
+
+//    for (int j = i + 1; j < arr.Length; j++)
+//    {
+//        if (arr[j] < arr[i])
+//        {
+//            int container = arr[i];
+//            arr[i] = arr[j];
+//            arr[j] = container;
+//        }
+
+//    }
+//}
+
+//int a=int.Parse(Console.ReadLine());
+//int sum = 0;
+
+//for (int i = 1;i<=a;i++)
+//{
+//    if (i % 2 == 0)
+//        sum++;
+//}
+//Console.WriteLine("girdiyiniz ededin icindeki 2 bolunenlerin sayi: "+sum);
+
+//girilen ededin faktoryali tapmag yeni 5 girilse 1vur2vur3vur4vur5 yeni girilen ededin pzude daxil
+//int a=int.Parse(Console.ReadLine());
+//int sum = 1;
+
+//for(int i=1;i <=a;i++)
+//{
+//    if(i<=a)
+//    {
+//        sum*=i;
+//    }
+//}
+//Console.WriteLine(sum);
+
+
+//int a = 10;
+//for(int i = 0;a != i*2;i++)
+//{
+//    Console.WriteLine("kenan");
+//}
+
+
+////int sayi=int.Parse(Console.ReadLine());
+////int faktorial = 1;
+////while(sayi>0)
+////{
+////    faktorial*=sayi;
+////    sayi--;
+////}
+////Console.WriteLine(faktorial);
+
+///
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
